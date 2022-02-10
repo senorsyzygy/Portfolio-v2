76d5370a074f220264faa93c0510b976e5da9c0c
senorsyzygy <45519112+senorsyzygy@users.noreply.github.com>
2022-02-10 10:42:50 +0000

    shifted blog down in layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Col, Row, Nav, Navbar, Container } from "react-bootstrap";
-import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Nav, Navbar, Container } from "react-bootstrap";
 import About from './About/About';
 import Home from './Home/Home';
 import Blog from './Blog/Blog';
 import Projects from './Projects/Projects';
-import Guestbook from './Guestbook/Guestbook';
 
 function App() {
 return (
@@ -29,15 +27,14 @@ return (
             <Nav.Link href="#pricing">Projects</Nav.Link>
             <Nav.Link href="#features">Blog</Nav.Link>
             <Nav.Link href="#features">Contact</Nav.Link>
-            <Nav.Link href="#features">Guestbook</Nav.Link>
         </Nav>
         </Container>
     </Navbar>
     <Home />
     <Container className="contentContainer">
         <About />
-        <Blog />
         <Projects />
+        <Blog />
     </Container>
     </main>
     </div>
